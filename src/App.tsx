2cacf2691e0cf59e8f6a0b41711690a930f32ba6
Regla Hermosín Rodríguez <rhermosinrodriguez@gmail.com>
2024-03-05 20:29:22 +0100

    Footer component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import "./App.css";
+import { HomeLogged } from "./pages/HomeLogged/HomeLogged";
 import Login from "./pages/Login/Login";
 
 function App() {
   return (
     <div>
-      <Login />
+      {/* <Login /> */}
+      <HomeLogged />
     </div>
   );
 }
