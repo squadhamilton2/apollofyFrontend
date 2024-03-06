5c6debfc23671bfef7333e0ca5493b068580d394
Ignasi CP <116747620+icplana@users.noreply.github.com>
2024-03-06 23:57:44 +0100

    album line component done

--- src/pages/HomeLogged/HomeLogged.tsx
+++ src/pages/HomeLogged/HomeLogged.tsx
@@ -3,6 +3,7 @@ import Welcome from "../../components/Welcome/Welcome";
 import UserMusic from "../../components/UserMusic/UserMusic";
 import "./homelogged.css";
 import { PlayingBar } from "../../components/PlayingBar/PlayingBar";
+import { AlbumLine } from "../../components/AlbumLine/AlbumLine";
 
 export const HomeLogged = () => {
   return (
