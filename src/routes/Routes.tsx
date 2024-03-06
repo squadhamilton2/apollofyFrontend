2916535b1755728b4c481c736012ed7f188e6ba9
Christian Banos <christiandepaulbp.@gmail.com>
2024-03-06 11:28:40 +0000

    create welcome page

--- src/routes/Routes.tsx
+++ src/routes/Routes.tsx
@@ -1,19 +1,18 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom"
-import Login from "../pages/Login/Login"
-import { HomeNotLogged } from "../pages/HomeNotLogged/HomeNotLogged"
-
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Login from "../pages/Login/Login";
+import { HomeNotLogged } from "../pages/HomeNotLogged/HomeNotLogged";
+import { HomeLogged } from "../pages/HomeLogged/HomeLogged";
 
 export const AppRoutes = () => {
   return (
     <>
-    <BrowserRouter>
-    <Routes>
-    <Route path="/" element={<HomeNotLogged/>} />
-    <Route path="/login" element={<Login/>} />
-    
-
-    </Routes>
-    </BrowserRouter>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<HomeNotLogged />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/home" element={<HomeLogged />} />
+        </Routes>
+      </BrowserRouter>
     </>
-  )
-}
+  );
+};
