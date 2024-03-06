3fe867c1b9a6a09af514dd31cd25116a2f5bd6b8
Ignasi CP <116747620+icplana@users.noreply.github.com>
2024-03-06 12:02:20 +0100

    register layout implemented

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,11 @@
 import './App.css'
 
-import { HomeNotLogged } from './pages/HomeNotLogged/HomeNotLogged'
 
 function App() {
   
 
   return (
     <div className='App'>
-      <HomeNotLogged />
     </div>
   )
 }
