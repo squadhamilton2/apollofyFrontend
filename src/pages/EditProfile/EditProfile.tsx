cc39560b69eb9c356b8ef36f9a605f32c85ef419
Regla Hermosín Rodríguez <rhermosinrodriguez@gmail.com>
2024-03-07 09:39:19 +0100

    Edit profile page without button logic

--- src/pages/EditProfile/EditProfile.tsx
+++ src/pages/EditProfile/EditProfile.tsx
@@ -28,16 +28,15 @@ export const EditProfile = () => {
   }
 
   return (
-    <>
+    <main className="profile_settings">
       <h1 className="edit_profile">Edit profile</h1>
 
       <form className="profile_form">
         <input className='name_inputForm' type="text" placeholder="Name"/>
-        
- 
+         
         <div className="date_selectors">
           <select className='profile_inputForm' value={day} onChange={handleDayChange}>
-            <option value="">Día</option>
+            <option value="">Day</option>
           
             {Array.from({length: 31}, (_, index) => index + 1).map(day => (
               <option key={day} value={day}>{day}</option>
@@ -45,7 +44,7 @@ export const EditProfile = () => {
           </select>
 
           <select className='profile_inputForm' value={month} onChange={handleMonthChange}>
-            <option value="">Mes</option>
+            <option value="">Month</option>
     
             {Array.from({length: 12}, (_, index) => index + 1).map(month => (
               <option key={month} value={month}>{month}</option>
@@ -53,26 +52,30 @@ export const EditProfile = () => {
           </select>
 
           <select className='profile_inputForm' value={year} onChange={handleYearChange}>
-            <option value="">Año</option>
+            <option value="">Year</option>
       
             {Array.from({length: currentYear - 1929}, (_, index) => 1930 + index).map(year => (
               <option key={year} value={year}>{year}</option>
             ))}
           </select>
+          </div>
+
+        <div className="country_container">
           <select className='profile_inputForm country_inputForm' value={country} onChange={handleCountryChange}>
-          <option value="">País</option>
-          <option value="USA">Estados Unidos</option>
-          <option value="Canada">Canadá</option>
-          <option value="Mexico">México</option>
-          <option value="Brazil">Brasil</option>
-          <option value="France">Francia</option>
-          <option value="Germany">Alemania</option>
-          <option value="Japan">Japón</option>
+          <option value="">Select country</option>
+          <option value="USA">USA</option>
+          <option value="Canada">Canada</option>
+          <option value="Mexico">Mexico</option>
+          <option value="Brazil">Brazi</option>
+          <option value="France">France</option>
+          <option value="Germany">Germany</option>
+          <option value="Japan">Japan</option>
           <option value="China">China</option>
           <option value="Australia">Australia</option>
-          <option value="Spain">España</option>
-        </select>
+          <option value="Spain">Spain</option>
+          </select>
         </div>
+        
 
         <div className="buttons">
           <button className="cancel_button">Cancel</button>
@@ -81,6 +84,6 @@ export const EditProfile = () => {
 
       </form>
       <Footer />
-    </>
+    </main>
   );
 };
