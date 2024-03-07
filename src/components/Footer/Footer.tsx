8440c8b457b58278087e153ef3fb8efe2b11595b
Regla Hermosín Rodríguez <rhermosinrodriguez@gmail.com>
2024-03-07 12:24:49 +0100

    Added links to footer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,12 +1,13 @@
 import { FaHome, FaSearch, FaCompactDisc } from "react-icons/fa";
 import "./footer.css"
+import { Link } from "react-router-dom";
 
 export default function Footer() {
   return (
     <div className="footer_container">
-      <FaHome className="home_icon"/>
-      <FaSearch className="home_icon"/>
-      <FaCompactDisc className="home_icon"/>
+      <Link to={"/"}><FaHome className="home_icon"/></Link>
+      <Link to={"/search"}><FaSearch className="home_icon"/></Link>
+      <Link to={"/songsSection"}><FaCompactDisc className="home_icon"/></Link>
     </div>
   )
 }
