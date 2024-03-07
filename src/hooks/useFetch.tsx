03e086846fc9c8d5e0770dcf8628c545c4850a48
Ignasi CP <116747620+icplana@users.noreply.github.com>
2024-03-07 10:36:49 +0100

    fetch hook implemented, no clg

--- src/hooks/useFetch.tsx
+++ src/hooks/useFetch.tsx
@@ -5,7 +5,5 @@ export const useFetchDBJSON = async () => {
     const response = await fetch('src/data/db.json')
     const data = await response.json()
 
-    console.log(data)
-
     return data
 }
--- src/pages/HomeLogged/HomeLogged.tsx
+++ src/pages/HomeLogged/HomeLogged.tsx
@@ -3,10 +3,9 @@ import Welcome from "../../components/Welcome/Welcome";
 import UserMusic from "../../components/UserMusic/UserMusic";
 import "./homelogged.css";
 import { PlayingBar } from "../../components/PlayingBar/PlayingBar";
-import { useFetchDBJSON } from "../../hooks/useFetch";
+
 
 export const HomeLogged = () => {
-  useFetchDBJSON()
   return (
     <>
       <Welcome />
