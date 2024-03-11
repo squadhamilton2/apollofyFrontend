ce6cb56171f0bafeabeff91349e89181e27de558
Earnen <p.wilmun@gmail.com>
2024-03-11 08:23:41 +0100

    made corrections to db.json and interfaces

--- src/interfaces/user.ts
+++ src/interfaces/user.ts
@@ -1,8 +1,10 @@
 export interface User {
   id: number;
+  username: string;
   first_name: string;
   last_name: string;
   email: string;
+  password: string;
   profilePicture: string;
   likedSongs: number[];
   likedArtist: number[];
